19e3deb35aac09fee5f3d8ca5fdda5ea5ba0d23c
Reka <reka.kovacs1993@gmail.com>
2024-02-04 11:16:21 +0000

    auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,9 @@
 import { authOptions } from "@/lib/auth";
 import NextAuth from "next-auth";
 
+authOptions.secret = process.env.NEXTAUTH_SECRET;
+
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
